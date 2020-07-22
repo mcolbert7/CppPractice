f6a411fbb646cb5512f3e8a41a558c4fcbe28851
Mia <miacolbert3@gmail.com>
2020-07-22 16:10:51 -0400

    Added incomplete GTS Interview Code

--- TripleStep.cpp
+++ TripleStep.cpp
@@ -1,2 +1,8 @@
 //This is my implementation of the dynamic programming/recursion problem Triple Step
+/*
+ * A child is running  up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time.
+ * Implement a method to count how many possible ways the child can run up the n stairs.
+ */
+
+
 
